4e8bb257ce78dcc4b76989c05b172b99ff60042f
Halil Safa SAĞLIK <35993436+ogeday26@users.noreply.github.com>
2019-09-03 21:55:48 +0300

    authentication/login schema added

---
+++ server/src/schemas/authentication/Login.js
@@ -0,0 +1,28 @@
+const Joi = require('joi');
+
+module.exports = (req, res, next) => {
+  const schema = Joi.object().keys({
+    email: Joi.string()
+      .email({ minDomainAtoms: 2 })
+      .required(),
+    password: Joi.string().regex(/^[a-zA-Z0-9!@#\$%\^\&*_=+-]{3,30}$/)
+  });
+
+  const { error } = Joi.validate(req.body, schema);
+
+  if (error) {
+    switch (error.details[0].context.key) {
+      case 'email':
+        return res.status(401).send('Girdiğiniz E-Posta doğru bir E-Posta adresi değil.');
+        break;
+      case 'password':
+        return res.status(401).send('Parola alanında desteklenmeyen karakter mevcut.');
+        break;
+      default:
+        console.log('V9999');
+        break;
+    }
+  } else {
+    next();
+  }
+};
