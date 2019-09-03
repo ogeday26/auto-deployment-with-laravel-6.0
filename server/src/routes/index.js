43990111d4e909fee422ef08b2f86c5d31a460f8
Halil Safa SAĞLIK <35993436+ogeday26@users.noreply.github.com>
2019-09-03 21:56:54 +0300

    index outes added authentication group file

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,3 +3,5 @@ const app = (module.exports = require('express')());
 app.get('/', (req, res) => {
   res.status(200).json('Welcome to Auto-deploy Rest Service');
 });
+
+app.use('/authentication', require('./authentication'));
