a2d7a58f862000bcea1940eec34e0537f188ffb6
Halil Safa SAĞLIK <35993436+ogeday26@users.noreply.github.com>
2019-09-05 02:19:16 +0300

    login schema error code change

--- server/src/schemas/authentication/Login.js
+++ server/src/schemas/authentication/Login.js
@@ -13,10 +13,10 @@ module.exports = (req, res, next) => {
   if (error) {
     switch (error.details[0].context.key) {
       case 'email':
-        return res.status(401).send('Girdiğiniz E-Posta doğru bir E-Posta adresi değil.');
+        return res.status(400).send('Girdiğiniz E-Posta doğru bir E-Posta adresi değil.');
         break;
       case 'password':
-        return res.status(401).send('Parola alanında desteklenmeyen karakter mevcut.');
+        return res.status(400).send('Parola alanında desteklenmeyen karakter mevcut.');
         break;
       default:
         console.log('V9999');
