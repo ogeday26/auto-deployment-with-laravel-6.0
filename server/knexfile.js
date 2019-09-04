b0c98c251f31913d2136521c7efb420c71bdf3d9
Halil Safa SAĞLIK <35993436+ogeday26@users.noreply.github.com>
2019-09-05 01:56:39 +0300

    knex file change for just development

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,50 +1,48 @@
-'use strict';
-
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
-      host: '127.0.0.1',
-      user: 'postgres',
-      password: 'ogeday26',
-      database: 'postgres'
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: `${__dirname}/database/migrations`
-    },
-    seeds: {
-      directory: `${__dirname}/database/seeds`
-    }
+  // development: {
+  client: 'pg',
+  connection: {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE
   },
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: process.env.POSTGRES_DATABASE,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: `${__dirname}/database/migrations`
   },
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: process.env.POSTGRES_DATABASE,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  seeds: {
+    directory: `${__dirname}/database/seeds`
   }
+  // }
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: process.env.POSTGRES_DATABASE,
+  //     user: process.env.POSTGRES_USER,
+  //     password: process.env.POSTGRES_PASSWORD
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: process.env.POSTGRES_DATABASE,
+  //     user: process.env.POSTGRES_USER,
+  //     password: process.env.POSTGRES_PASSWORD
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
 };
